9075438af8eca36b654fb5e6d5023722d33624f4
Cristian Fernández Rodríguez <mcferrod19@gmail.com>
2017-05-24 12:29:43 +0200

    button work

---
+++ js/header.js
@@ -0,0 +1,12 @@
+var menuButton = document.querySelector('.buttonMenu');
+
+function toggleVisibility() {
+  var menu = document.querySelector(".buttonContainerMenu");
+  if(menu.classList.contains("hidden")){
+    menu.classList.remove("hidden");
+  }else{
+    menu.classList.add("hidden");
+  }
+}
+
+menuButton.addEventListener('click', toggleVisibility);
